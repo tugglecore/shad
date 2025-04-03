a153c2637ffa471f10962dc15a0fda1a7b33aa08
tuggle <tuggledemetrius@gmail.com>
2025-04-02 23:18:52 -0400

    wip single filter line

--- src/filter.rs
+++ src/filter.rs
@@ -21,6 +21,7 @@ struct Filter {
 #[derive(Debug, Default)]
 pub struct FilterBox {
     pub input: String,
+    filter_area: Rect,
     cursor_position: [u16; 2],
 }
 
@@ -39,62 +40,67 @@ impl FilterBox {
             [Constraint::Length(1), Constraint::Fill(1)],
         )
         .split(area);
-
-        let label_text = "Filters";
-        let label_widget =
-            Text::from(label_text).style(Style::new().add_modifier(Modifier::UNDERLINED));
-
-        let mut label_area = splits[0];
-        label_area.width = label_text.len() as u16;
-
-        frame.render_widget(label_widget, label_area);
-
         let filter_area = splits[1];
+        self.filter_area = filter_area;
+
+        let label_text = "Filters: ";
+        let input_box_splits = Layout::new(
+            Direction::Horizontal,
+            [
+                Constraint::Length(label_text.len() as u16),
+                Constraint::Fill(1),
+            ],
+        )
+        .split(splits[0]);
+
+        let label_widget = Text::from(label_text);
+
+        frame.render_widget(label_widget, input_box_splits[0]);
 
         if self.input.is_empty() && !currently_filtering {
             let help_text = "Press f to add filters";
             let help_widget = Text::from(help_text);
-            frame.render_widget(help_widget, filter_area);
+            frame.render_widget(help_widget, input_box_splits[1]);
 
             return;
         }
 
-        let mut lines = vec![];
-
-        dbg!(&self);
-        dbg!(&filter_area);
-
-        dbg!(self.input.len());
-
-        let mut lower_bound = 0;
-        while lower_bound < self.input.len() {
-            let filter_width = filter_area.width as usize;
-            
-            let mut upper_bound = lower_bound + (filter_width - 1);
-            if upper_bound >= self.input.len() {
-                upper_bound = self.input.len() - 1
-            }
-            // let upper_bound = if upper_bound < self.input.len() {
-            //     position + filter_width
-            // } else {
-            //     position + self.input.len() - 1
-            // };
-
-            let filter_line = Line::from(
-                &self.input[lower_bound..=upper_bound]
-            );
-
-            lines.push(filter_line);
-
-            lower_bound = upper_bound + 1;
-        }
-
-        frame.render_widget(Text::from(lines), filter_area);
-
-        frame.set_cursor_position(Position::new(
-            filter_area.x + self.cursor_position[0],
-            filter_area.y + self.cursor_position[1]
-        ));
+        // let mut lines = vec![];
+        //
+        // dbg!(&self);
+        // dbg!(&filter_area);
+        //
+        // dbg!(self.input.len());
+        //
+        // let mut lower_bound = 0;
+        // while lower_bound < self.input.len() {
+        //     let filter_width = filter_area.width as usize;
+        //
+        //     let mut upper_bound = lower_bound + (filter_width - 1);
+        //     if upper_bound >= self.input.len() {
+        //         upper_bound = self.input.len() - 1
+        //     }
+        //     // let upper_bound = if upper_bound < self.input.len() {
+        //     //     position + filter_width
+        //     // } else {
+        //     //     position + self.input.len() - 1
+        //     // };
+        //
+        //     let filter_line = Line::from(
+        //         &self.input[lower_bound..=upper_bound]
+        //     );
+        //
+        //     lines.push(filter_line);
+        //
+        //     lower_bound = upper_bound + 1;
+        // }
+        //
+        // frame.render_widget(Text::from(lines), filter_area);
+        //
+        // frame.set_cursor_position(Position::new(
+        //     filter_area.x + self.cursor_position[0],
+        //     filter_area.y + self.cursor_position[1]
+        // ));
     }
 
     pub fn on_key_event(&mut self, key: KeyEvent) {
